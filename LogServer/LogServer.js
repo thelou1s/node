1c6eff81f6a624fe056dcdbe8123e01a59d8a25e
Luis <thelou1s@yahoo.com>
2016-10-01 09:55:20 +0800

    insert from browser ok

--- LogServer/LogServer.js
+++ LogServer/LogServer.js
@@ -12,7 +12,7 @@ var db = new sql.Database(DB_FILE);
 var onSerialize = function() {
 	if(dbExists) {
 		create();
-		insert("time 1", "tag 1", "log 1");
+		//insert("time 1", "tag 1", "log 1");
 		queryAll();
 	}
 
@@ -28,16 +28,16 @@ function create() {
 
 function insert(time, tag, log) {
 	
+	console.log("insert() " + time + ", " + tag + ", " + log);
 	var SQL_INSERT_INTO = db.prepare("insert into log_table(_time, _tag, _log) values(?, ?, ?)");
 	SQL_INSERT_INTO.run(time, tag, log);
 	SQL_INSERT_INTO.finalize();
-	console.log("insert ok");
 
 }
 
 function queryAll() {
 	db.each(SQL_SELECT_ALL, function(err, row) {
-		console.log("queryAll " + row._time + ", " + row._tag + ", " + row._log);
+		console.log("queryAll " + row._id + ", " + row._time + ", " + row._tag + ", " + row._log);
 	});
 }
 
@@ -45,12 +45,48 @@ function queryAll() {
 db.serialize(onSerialize);
 
 
-var server = http.createServer(function (req, res){
-	res.writeHead(200, {"Content-Type": "text/plain"});
-	res.end("Hello World");
-	console.log("createServer ok");
-});
+var url = require("url");
+var SERVER_PORT = 8080;
 
-server.listen(8080);
+var server = http.createServer(function onRequest(req, res) {
+	//console.log("onRequest " + toString.call(req));
+	console.log(".");
+	console.log(".");
+	
+	res.write("Hello World2");
+	res.end();
+
+	router(req);
+});
+	
+
+function router(req) {
+	console.log("router() http://localhost:" + SERVER_PORT + req.url);
+	var isFavIcon = req.url == "/favicon.ico";
+	console.log("router() " + isFavIcon);
+        if (isFavIcon) {
+		return;
+	}
+
+	var pathname = url.parse(req.url).pathname;
+	console.log("router() " + pathname);
+
+	if(pathname == "/insert") {
+		var query = url.parse(req.url).query;
+		var qs = require('querystring');
+		var q = qs.parse(query);
+		var table = q["table"];
+		var time = q["time"];
+		var tag = q["tag"];
+		var log = q["log"];
+		console.log("router() " + table + ", " + time + ", " + tag + ", " + log);
+
+		insert(time, tag, log);
+		queryAll();
+	}
+}
+
+server.listen(SERVER_PORT);
 
 console.log("server listenning on 8080");
+console.log("======================================================");
